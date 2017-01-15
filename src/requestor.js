7610b7c5b2918b81af0f587bd5c82edf2c964895
David Nagli <davidnagli@gmail.com>
2017-01-15 18:37:39 -0500

    Added POST to requestor

--- src/requestor.js
+++ src/requestor.js
@@ -17,6 +17,23 @@ requestor.get = (url) => {
     })
 };
 
+requestor.post = (url, data) => {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.open("POST", url);
+        request.onreadystatechange = function () {
+            if(request.readyState === 4){
+                if(request.status === 200){
+                    resolve(request.response)
+                }else{
+                    reject({code: request.status, content: request.response})
+                }
+            }
+        };
+        request.send(data);
+    })
+};
+
 requestor.getAPIData = (url) => {
     return new Promise((resolve, reject) => {
         requestor.get(url).then(JSON.parse).then(res => {

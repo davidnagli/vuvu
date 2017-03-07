62097f3e20096623ec5f8209affd25f98793ee88
davidnagli <davidnagli@gmail.com>
2017-03-07 10:38:05 -0500

    Added vuvuFromAPI

--- src/vuvu.js
+++ src/vuvu.js
@@ -1,6 +1,30 @@
-function vuvu(data){
-    return new Vue({
-        el: "#app",
-        data
-    })
+function vuvu(data) {
+  return new Vue({
+    el: "#app",
+    data
+  })
 };
+
+function vuvuFromAPI(url, key = "requestData") {
+  //add v-cloak
+  document.getElementById("app").setAttribute("v-cloak", "")
+
+  //add css v-cloak hide
+	var style = document.createElement('style');
+
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = "[v-cloak] { display: none; }";
+  } else {
+    style.appendChild(document.createTextNode("[v-cloak] { display: none; }"));
+  }
+
+  document.head.appendChild(style);
+
+  //actually get the data
+  data = {}
+  requestor.getAPIData(url).then(value => {
+    data[key] = value
+    vuvu(data)
+  })
+}
